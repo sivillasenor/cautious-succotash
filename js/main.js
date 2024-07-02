0a4c999ba4ebdd60b87876773b2041f09b04a589
sivillasenor <sivillasenor@csuchico.edu>
2024-07-02 23:05:47 +0000

    js things

--- js/main.js
+++ js/main.js
@@ -1,5 +1,19 @@
-$( document ).ready(function() {
-    $('#btn').click(function() {
-        window.alert("DID YOU JUST CLICK ME?!");
-    });
-});
+// let = array = [];
+
+// for(let i = 0; i < 10); i ++){
+ //   array.push(i);
+//}
+
+//console.log(array);
+
+//for(let i = 0; i < 10; i++){
+//    console.log(array[i]);
+// }
+
+let nameInput = prompt("What is your name?");
+
+function greeting(name){
+    console.log("hello " + name + " today is tuesday.");
+}
+
+greeting(nameInput);
